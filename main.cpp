1c4830345846aa320634422e07fec373bf09cfa2
Ingwar Smith <ingwarsmith@yandex.ru>
2021-05-08 13:41:30 +0500

    * std::chrono для замера времени работы * коррекция алгоритма для std::thread
    Signed-off-by: Ingwar Smith <ingwarsmith@yandex.ru>

--- main.cpp
+++ main.cpp
@@ -7,14 +7,28 @@
 #include <regex>
 #include <sstream>
 
-#include <omp.h>
+#include <chrono>
 
-#define parallel_cfg_omp
+//#define parallel_cfg_omp
+
+#ifndef parallel_cfg_omp
+#define parallel_cfg_std_thread
+#include <thread>
+#else
+#include <omp.h>
+#endif
 
 namespace fs = std::filesystem;
 
+void processFile(std::filesystem::path p, const std::vector<std::pair<std::string, std::string>> replacing_table);
+
 int main()
 {
+    auto ms = std::chrono::duration_cast< std::chrono::milliseconds >(
+            std::chrono::system_clock::now().time_since_epoch()
+                );
+    auto tm_beg = ms.count();
+
     std::ifstream configuration { "config.ini" };
     if (configuration.fail())
     {
@@ -41,6 +55,11 @@ int main()
     cfg_content[0].replace(pos, threadsNum.size(), "");
 #ifdef parallel_cfg_omp
     omp_set_num_threads(std::stoi(cfg_content[0]));
+#endif
+
+#ifdef parallel_cfg_std_thread
+    const size_t threads_num = std::stoul(cfg_content[0]);
+    std::vector<std::thread> work_threads;
 #endif
 
     std::string workPth("work_path:");
@@ -85,53 +104,115 @@ int main()
 
     std::cout << files.size() << " files" << std::endl;
 
-#ifdef parallel_cfg_omp
-#pragma omp parallel for
-#endif
-    for (size_t f_idx = 0; f_idx < files.size(); ++f_idx)
+#ifdef parallel_cfg_std_thread
+
+    bool started_thread[threads_num];
+    size_t f_idx = 0;
+    for (size_t thr_idx = 0; thr_idx < threads_num; ++thr_idx)
     {
-        fs::path p = files[f_idx];
-        auto filename_str = p.filename().generic_string();
-        std::ifstream in_stream_txt { p.c_str() };
+        std::thread th_new { processFile, files[f_idx], replacing_table };
+        work_threads.push_back(std::move(th_new));
+        started_thread[thr_idx] = true;
 
-        std::vector<std::string> content;
-        std::string str;
-        auto found = false;
-        while (!in_stream_txt.eof())
+        ++f_idx;
+    }
+    while (true)
+    {
+        for (size_t thr_idx = 0; thr_idx < threads_num && f_idx < files.size(); ++thr_idx)
         {
-            std::getline(in_stream_txt, str);
-
-            for (auto it = replacing_table.cbegin(); it != replacing_table.cend(); ++it)
+            if (started_thread[thr_idx])
             {
-                std::string tmpl = it->first;
-                if (std::regex_search(std::begin(str), std::end(str), std::regex(tmpl)))
+                if (work_threads[thr_idx].joinable())
                 {
-                    found = true;
-                    size_t pos = str.find(tmpl);
-                    while (pos != std::string::npos)
-                    {
-                        str.replace(pos, tmpl.size(), it->second);
-                        pos = str.find(tmpl, pos);
-                    }
+                    work_threads[thr_idx].join();
+                    started_thread[thr_idx] = false;
+                }
+                else continue;
+            }
+            if (!started_thread[thr_idx])
+            {
+                std::thread th_new { processFile, files[f_idx], replacing_table };
+                work_threads[thr_idx] = std::move(th_new);
+                started_thread[thr_idx] = true;
+                f_idx++;
+            }
+        }
+
+        if (f_idx >= files.size())
+        {
+            while (work_threads.size())
+            {
+                if (work_threads.front().joinable())
+                {
+                    work_threads.front().join();
+                    work_threads.erase(work_threads.begin());
                 }
             }
 
-            content.emplace_back(std::move(str));
+            break;
         }
-        in_stream_txt.close();
-
-        if (found)
-        {
-            std::ofstream out_stream_txt { p.c_str() };
-            for (auto it = content.cbegin(); it != content.cend(); ++it)
-            {
-                out_stream_txt << *it << "\n";
-            }
-            out_stream_txt.close();
-        }
-
-        content.clear();
     }
 
+#else
+#ifdef parallel_cfg_omp
+#pragma omp parallel for //shared(files, replacing_table)
+#endif
+    for (size_t f_idx = 0; f_idx < files.size(); ++f_idx)
+    {
+        processFile(files[f_idx], replacing_table);
+    }
+
+#endif
+
+    ms = std::chrono::duration_cast< std::chrono::milliseconds >(
+                std::chrono::system_clock::now().time_since_epoch()
+                    );
+    auto tm_end = ms.count();
+
+    std::cout << "work is finished by " << (tm_end - tm_beg) << " msec\n";
+
     return 0;
+}
+
+void processFile(std::filesystem::path p, const std::vector<std::pair<std::string, std::string> > replacing_table)
+{
+    std::ifstream in_stream_txt { p.c_str() };
+
+    std::vector<std::string> content;
+    std::string str;
+    auto found = false;
+    while (!in_stream_txt.eof())
+    {
+        std::getline(in_stream_txt, str);
+
+        for (auto it = replacing_table.cbegin(); it != replacing_table.cend(); ++it)
+        {
+            std::string tmpl = it->first;
+            if (std::regex_search(std::begin(str), std::end(str), std::regex(tmpl)))
+            {
+                found = true;
+                size_t pos = str.find(tmpl);
+                while (pos != std::string::npos)
+                {
+                    str.replace(pos, tmpl.size(), it->second);
+                    pos = str.find(tmpl, pos);
+                }
+            }
+        }
+
+        content.emplace_back(std::move(str));
+    }
+    in_stream_txt.close();
+
+    if (found)
+    {
+        std::ofstream out_stream_txt { p.c_str() };
+        for (auto it = content.cbegin(); it != content.cend(); ++it)
+        {
+            out_stream_txt << *it << "\n";
+        }
+        out_stream_txt.close();
+    }
+
+    content.clear();
 }
